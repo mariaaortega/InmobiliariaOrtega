31021228a104770e933f5fa033d8e2eb902216f6
antuo <antuo@ANTONELLA>
2022-03-28 19:13:53 -0300

    Agregar archivos de proyecto.

---
+++ InmobiliariaOrtega/Models/Propietario.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InmobiliariaOrtega.Models
+{
+    public class Propietario
+    {
+        public int IdPropietario { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio"),
+            MaxLength(50, ErrorMessage = "Máximo 50 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio"),
+            MaxLength(50, ErrorMessage = "Máximo 50 caracteres")]
+        public string Apellido { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio"),
+            StringLength(8, MinimumLength = 8, ErrorMessage = "Un DNI debe tener 8 dígitos")]
+        public string Dni { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio"),
+            StringLength(15, MinimumLength = 10, ErrorMessage = "Un número de teléfono debe tener entre 10 y 15 dígitos")]
+        public string Telefono { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio"),
+            EmailAddress(ErrorMessage = "Debe ser una dirección de correo válida"),
+            MaxLength(50, ErrorMessage = "Máximo 50 caracteres")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio"),
+            MaxLength(50, ErrorMessage = "Máximo 50 caracteres"),
+            DataType(DataType.Password)]
+        public string Clave { get; set; }
+    }
+}
---
+++ InmobiliariaOrtega/Models/RepositorioPropietario.cs
@@ -0,0 +1,65 @@
+﻿using System.Data.SqlClient;
+
+namespace InmobiliariaOrtega.Models
+{
+    public class RepositorioPropietario
+    {
+        string ConnectionString = "Server=(localdb)\\MSSQLLocalDB;Database=Inmobiliaria2022;Trusted_Connection=True;MultipleActiveResultSets=true";
+        public RepositorioPropietario()
+        { }
+		public IList<Propietario> ObtenerTodos()
+		{
+			var res = new List<Propietario>();
+			using (SqlConnection connection = new SqlConnection(ConnectionString))
+			{
+				string sql = $"SELECT IdPropietario, Nombre, Apellido, Dni, Telefono, Email, Clave" +
+					$" FROM Propietarios;";
+				using (SqlCommand comm = new SqlCommand(sql, connection))
+				{
+					connection.Open();
+					var reader = comm.ExecuteReader();
+					while (reader.Read())
+					{
+						Propietario p = new Propietario
+						{
+							IdPropietario = reader.GetInt32(0),
+							Nombre = reader.GetString(1),
+							Apellido = reader.GetString(2),
+							Dni = reader.GetString(3),
+							Telefono = reader.GetString(4),
+							Email = reader.GetString(5),
+							Clave = reader.GetString(6),
+						};
+						res.Add(p);
+					}
+					connection.Close();
+				}
+			}
+			return res;
+		}
+		//{}
+		public int Alta(Propietario p)
+		{
+			var res = -1;
+			using (SqlConnection connection = new SqlConnection(ConnectionString))
+			{
+				 string sql = $"INSERT INTO Propietarios (Nombre, Apellido, Dni, Telefono, Email, Clave) " +
+					$"VALUES (@nombre, @apellido, @dni, @telefono, @email, @clave);" +
+					"SELECT SCOPE_IDENTITY();";
+				using (SqlCommand comm = new SqlCommand(sql, connection)) {
+					comm.Parameters.AddWithValue("@nombre", p.Nombre);
+					comm.Parameters.AddWithValue("@apellido", p.Apellido);
+					comm.Parameters.AddWithValue("@dni", p.Dni);
+					comm.Parameters.AddWithValue("@telefono", p.Telefono);
+					comm.Parameters.AddWithValue("@email", p.Email);
+					comm.Parameters.AddWithValue("@clave", p.Clave);
+					connection.Open();
+					res = Convert.ToInt32(comm.ExecuteScalar());
+					p.IdPropietario = res;
+					connection.Close();
+				}
+			}
+			return res;
+		}
+	}
+}
